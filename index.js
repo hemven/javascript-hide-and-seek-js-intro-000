989c6304d8775c466d93013df528207865aaa767
Hema Venkata <hvenkata@live.com>
2019-07-08 21:59:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,6 +23,13 @@ function deepestChild(){
 //while (nextStop) {
 //  starter = nextStop;
 //  nextStop = starter.children[0];
+let node = document.getElementById('grand-node');
+let nextNode = node.children[0];
+
+while (nextNode) {
+  node = nextNode;
+  nextNode = node.children[0];
+
 }
 
 }
